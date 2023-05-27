69f98caabb69e0f74ee166969e35d67319f00d01
ajwdmedia <ajwdmedia@gmail.com>
2023-05-27 10:03:17 +0000

    this should work

--- src/routes/api/check_install/+server.ts
+++ src/routes/api/check_install/+server.ts
@@ -1,7 +1,6 @@
 import { dalai } from "../../dalai.server";
+import type { RequestHandler } from "./$types";
 
-export const GET = async (req, res) => {
-    if (dalai.installed.length > 0) {
-        
-    }
-}
+export const GET = (async (req, res) => {
+    res.json({ has_alpaca: (await dalai.installed()).length > 0 })
+}) satisfies RequestHandler
